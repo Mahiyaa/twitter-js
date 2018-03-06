2aea9dd61de21a5f90f6d24761fc336e9c5d42eb
Mahiyaa <mmahia1121@gmail.com>
2018-03-06 11:12:40 -0500

    first portion complete

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 const express = require("express");
 const app = express(); // creates an instance of an express application
+
+app.use(function (req, res, next) {
+    console.log(req.method, req.originalUrl);
+    next();
+  });
+
+
+app.get("/", (req, res) => {
+    res.send("WELCOME!!!");
+});
+
+app.get("/news", (req, res) => {
+    res.send("DAILY NEWS");
+});
+
+
+
+
+app.listen(3000, ()=> {
+    console.log("Server listening...");
+});
