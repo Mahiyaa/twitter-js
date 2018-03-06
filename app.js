3af2472e23ddc3ea8cb83f29e4516196c016d840
Mahiyaa <mmahia1121@gmail.com>
2018-03-06 12:47:20 -0500

    updated

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 const app = express(); // creates an instance of an express application
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+//Turn off Nunjuck's caching-------^
+const people = [ {name: 'Nunjucks'}, {name: 'MTA'}, {name: "People who talk too much during lectures"}, {name: 'People with GIANTTT a** backpacks on the train...'}, {name: 'People who dont bathe...'} ];
+
+app.get("/", (req, res) => {
+    res.render( 'index', {title: 'HALL OF SHAME', people: people} );
+});
+
 
 app.use(function (req, res, next) {
     console.log(req.method, req.originalUrl);
@@ -7,15 +19,13 @@ app.use(function (req, res, next) {
   });
 
 
-app.get("/", (req, res) => {
-    res.send("WELCOME!!!");
-});
+// app.get("/", (req, res) => {
+//     res.send("WELCOME!!!");
+// });
 
 app.get("/news", (req, res) => {
     res.send("DAILY NEWS");
 });
-
-
 
 
 app.listen(3000, ()=> {
